246026f91269c820eadc28cf8521a7201538e3d2
JeremyGaibor <gaiborrodriguezjeremy@gmail.com>
2025-09-22 14:58:25 -0500

    Correccion - Gaibor

--- Tienda_Tech/Tienda_Tech/src/main/java/com/example/tienda_tech/service/EmailService.java
+++
@@ -1,5 +0,0 @@
-// src/main/java/com/example/tienda_tech/service/EmailService.java
-package com.example.tienda_tech.service;
-
-// src/main/java/com/example/tienda_tech/service/EmailService.java
-public interface EmailService { void enviarCredenciales(String to, String usuario, String password); }
